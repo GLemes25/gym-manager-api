78dd6444fe3f2705ad3014480817ccab70861596
GLemes25 <gabriellemes924@gmail.com>
2026-03-01 19:49:55 -0400

    chore: update Prisma schema and auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,10 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { openAPI } from "better-auth/plugins";
-import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaClient } from "../generated/prisma/index.js";
 
-const prisma = new PrismaClient({
+export const prisma = new PrismaClient({
   adapter: new PrismaPg({
     connectionString: process.env.DATABASE_URL,
   }),
